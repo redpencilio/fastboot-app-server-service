76fefeeb346b3606e21d7d9480d159ac953dceae
achraf <achrafatauil@hotmail.com>
2020-09-10 16:16:45 +0200

    Add BASE_URL enviroment

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 let FastbootAppServer = require('fastboot-app-server');
 
+// Docker Enviroment Variables
+const BASE_URL = process.env.BASE_URL
+
 let fastbootAppServer = new FastbootAppServer({
   port: 80,
   distPath: "/app/",
   chunkedResponse: true,
-  sandboxGlobals: { BACKEND_URL: "http://backend/" }
+  sandboxGlobals: { 
+    BACKEND_URL: "http://backend/",
+    BASE_URL: BASE_URL
+   }
 });
 
 fastbootAppServer.start();
