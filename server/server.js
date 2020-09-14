acd454daf527aeb9752bf0a3247d3e1e1490fb87
achraf <achrafatauil@hotmail.com>
2020-09-14 13:32:11 +0200

    Fastboot replace env variables
    Search and replace base url docker-compose environment variable

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 let FastbootAppServer = require('fastboot-app-server');
+const fs  = require('fs')
 
 // Docker Enviroment Variables
 const BASE_URL = process.env.BASE_URL
+let indexFile = fs.readFileSync("/app/index.html", "utf8")
+fs.writeFileSync("/app/index.html", indexFile.replace(/EMBER_METIS_BASE_URL/g, encodeURIComponent(BASE_URL)))
 
 let fastbootAppServer = new FastbootAppServer({
   port: 80,
@@ -9,7 +12,6 @@ let fastbootAppServer = new FastbootAppServer({
   chunkedResponse: true,
   sandboxGlobals: { 
     BACKEND_URL: "http://backend/",
-    BASE_URL: BASE_URL
    }
 });
 
