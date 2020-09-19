2064f013f6cdc407bd4c5dfce0b644c2ffc137b7
Aad Versteden <madnificent@gmail.com>
2020-09-19 18:54:59 +0200

    Add missing semicolons

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 let FastbootAppServer = require('fastboot-app-server');
-const fs  = require('fs')
+const fs  = require('fs');
 
 // Docker Enviroment Variables
-const BASE_URL = process.env.BASE_URL
-let indexFile = fs.readFileSync("/app/index.html", "utf8")
-fs.writeFileSync("/app/index.html", indexFile.replace(/EMBER_METIS_BASE_URL/g, encodeURIComponent(BASE_URL)))
+const BASE_URL = process.env.BASE_URL;
+let indexFile = fs.readFileSync("/app/index.html", "utf8");
+fs.writeFileSync("/app/index.html", indexFile.replace(/EMBER_METIS_BASE_URL/g, encodeURIComponent(BASE_URL)));
 
 let fastbootAppServer = new FastbootAppServer({
   port: 80,
