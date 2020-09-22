030733d10aec8c6de803dea550d954b695ed97cf
Aad Versteden <madnificent@gmail.com>
2020-09-22 11:24:15 +0200

    Replace all environment variables starting with EMBER_
    All environment variables which start with EMBER_ should be replaced.
    The new implementation loops over all environment variables and
    actively replaces the necessary content.

--- server/server.js
+++ server/server.js
@@ -2,9 +2,17 @@ let FastbootAppServer = require('fastboot-app-server');
 const fs  = require('fs');
 
 // Docker Enviroment Variables
-const BASE_URL = process.env.BASE_URL;
-let indexFile = fs.readFileSync("/app/index.html", "utf8");
-fs.writeFileSync("/app/index.html", indexFile.replace(/EMBER_METIS_BASE_URL/g, encodeURIComponent(BASE_URL)));
+let indexHtmlFile = fs.readFileSync("/app/index.html", "utf8");
+for( const envVar in process.env ) {
+  console.log(`Processing ${envVar}...`);
+  if( envVar.indexOf("EMBER_") === 0 ) {
+    console.log(`Replacing name ${envVar} with value ${process.env[envVar]}`);
+    indexHtmlFile = indexHtmlFile.replace( new RegExp(envVar, "g"), encodeURIComponent(process.env[envVar]) );
+  } else {
+    console.log(`Environment variable ${envVar} not recognized for replacement.  Only variables starting with EMBER_ are used.`);
+  }
+}
+fs.writeFileSync("/app/index.html", indexHtmlFile);
 
 let fastbootAppServer = new FastbootAppServer({
   port: 80,
