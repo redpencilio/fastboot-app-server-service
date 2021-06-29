d098042caa83e9ac9c21c4cfe6e41aea7aee0225
Niels Vandekeybus <progster@gmail.com>
2021-06-29 22:40:28 +0200

    fix environment replacement

--- server/server.js
+++ server/server.js
@@ -4,13 +4,13 @@ const fs  = require('fs');
 // Docker Enviroment Variables
 const PREFIX = "EMBER_";
 const EMBER_CONFIG = Object.entries(process.env)
-      .filter((key) => key.indexOf(PREFIX) === 0)
-      .map((key) => [key.slice(PREFIX.length), process.env[key]]);
+      .filter(([key]) => key.indexOf(PREFIX) === 0)
+      .map(([key, value]) => [key.slice(PREFIX.length), value]);
 
 let indexHtml = fs.readFileSync("/app/index.html", "utf8");
 for (const [key, value] of EMBER_CONFIG) {
   console.log(`Replacing name ${key} with value ${value}`);
-  indexHtml = indexHtml.replace( new RegExp(key, "g"), encodeURIComponent(value) );
+  indexHtml = indexHtml.replace( new RegExp(`{{${key}}}`, "g"), encodeURIComponent(value) );
 }
 fs.writeFileSync("/app/index.html", indexHtml);
 
