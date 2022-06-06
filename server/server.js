3555751e6962fb8e9439cbc3126e89b3d4772b45
Niels V <niels.vandekeybus@redpencil.io>
2022-06-06 10:13:09 +0200

    create boot.sh and move env var replacement to it
    fastbootserver forks the node process and causes the replacement to happen in
    parraller, which causes a possible race condition resulting in a broken
    package.json file. by executing the replacement in a sh script we avoid this

--- server/server.js
+++ server/server.js
@@ -1,23 +1,6 @@
 const fs  = require('fs');
 const FastbootAppServer = require('fastboot-app-server');
 
-// Docker Enviroment Variables
-const PREFIX = "EMBER_";
-const EMBER_CONFIG = Object.entries(process.env)
-      .filter(([key]) => key.indexOf(PREFIX) === 0)
-      .map(([key, value]) => [key.slice(PREFIX.length), value]);
-
-let indexHtml = fs.readFileSync("/app/index.html", "utf8");
-let packageJson = fs.readFileSync("/app/package.json", "utf8");
-for (const [key, value] of EMBER_CONFIG) {
-  console.log(`Replacing name ${key} with value ${value}`);
-  packageJson = packageJson.replace( new RegExp(`{{${key}}}`, "g"), value);
-  indexHtml = indexHtml.replace( new RegExp(`{{${key}}}`, "g"), encodeURIComponent(value) );
-  indexHtml = indexHtml.replace( new RegExp(`%7B%7B${key}%7D%7D`, "g"), encodeURIComponent(value) );
-}
-fs.writeFileSync("/app/index.html", indexHtml);
-fs.writeFileSync("app/package.json", packageJson);
-
 let fastbootAppServer = new FastbootAppServer({
   port: 80,
   distPath: "/app/",
