d665ab40620195cc77aeffa98d61ce295940975e
abeforgit <arnebertrand@gmail.com>
2023-11-02 22:54:40 +0100

    feat(config): add support for ember-data >=4.12
    See reasoning and suggested fix [here](https://github.com/emberjs/data/issues/8475)

--- server/server.js
+++ server/server.js
@@ -8,11 +8,23 @@ let fastbootAppServer = new FastbootAppServer({
   gzip: true,
   log: true,
   buildSandboxGlobals(defaultGlobals) {
-    return Object.assign(
-      {},
-      defaultGlobals,
-        {BACKEND_URL: "http://backend"}
-    );
+    return Object.assign({}, defaultGlobals, {
+      AbortController,
+      ReadableStream:
+        typeof ReadableStream !== "undefined"
+          ? ReadableStream
+          : require("node:stream/web").ReadableStream,
+      WritableStream:
+        typeof WritableStream !== "undefined"
+          ? WritableStream
+          : require("node:stream/web").WritableStream,
+      TransformStream:
+        typeof TransformStream !== "undefined"
+          ? TransformStream
+          : require("node:stream/web").TransformStream,
+      Headers: typeof Headers !== "undefined" ? Headers : undefined,
+      BACKEND_URL: "http://backend",
+    });
   },
 });
 
